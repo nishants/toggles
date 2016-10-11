3b1a6b25991682abba3f3fc867dc7ce58e593670
nishant <nishant.singh87@gmail.com>
2016-10-11 13:13:41 +0530

    test for watcher.

--- test/TogglesSpec.js
+++ test/TogglesSpec.js
@@ -20,3 +20,21 @@ it('should have span element', function () {
   expect(createdElement).toBeDefined();
   expect(createdElement.text()).toEqual('This span is appended from directive.');
 });
+
+it('should have updated text in span', function (){
+  scope.text = 'some other text';
+  scope.$digest();
+  var createdElement = compile(scope, angular.element('<div feature-if ></div>')).find('span');
+  expect(createdElement).toBeDefined();
+  expect(createdElement.text()).toEqual(scope.text);
+});
+
+it('should increment value on click of button', function () {
+  scope.value=10;
+  var button = compile(scope, angular.element('<div feature-else ></div>')).find('button');
+
+  button.triggerHandler('click');
+  scope.$digest();
+
+  expect(scope.value).toEqual(11);
+});
