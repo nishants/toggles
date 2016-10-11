fd0b81bd71e58cdf87499133082b8a1949dbeef0
nishant <nishant.singh87@gmail.com>
2016-10-11 16:38:53 +0530

    remove element if feature is disbaled.

--- test/TogglesSpec.js
+++ test/TogglesSpec.js
@@ -1,40 +1,50 @@
 var scope,
     compile ,
-    DOM;
+    service,
+    DOM,
+    enabledElement,
+    disabledElement;
 
 beforeEach(function(){
   module('toggles');
 
-  inject(function($compile, $rootScope){
+  inject(function($compile, $rootScope, toggles, $q){
+    service = toggles;
+    scope   = $rootScope.$new();
+
+    var features = {
+      "default": {
+        "url"       : "localhost",
+        "features"  : {
+          "espp"    :false,
+          "pre-ipo" : true
+        }
+      }
+    };
+
+    var promiseOf =  function(data){
+      return $q(function(resolve, reject) {
+        resolve(data);
+      })
+    };
+
+    toggles.init(promiseOf(features));
+
     compile = function(scope, element){
       return $compile(element)(scope);
     };
-    scope = $rootScope.$new();
   });
 
+  DOM = compile(scope, angular.element('' +
+      '<div>' +
+          '<span class="espp" feature="espp"></span>' +
+      '</div>'
+  ));
+
+  enabledElement = DOM.find('span');
   scope.$digest();
 });
 
-it('should have span element', function () {
-  var createdElement = compile(scope, angular.element('<div feature></div>')).find('span');
-  expect(createdElement).toBeDefined();
-  expect(createdElement.text()).toEqual('This span is appended from directive.');
-});
-
-it('should have updated text in span', function (){
-  scope.text = 'some other text';
-  scope.$digest();
-  var createdElement = compile(scope, angular.element('<div feature-if ></div>')).find('span');
-  expect(createdElement).toBeDefined();
-  expect(createdElement.text()).toEqual(scope.text);
-});
-
-it('should increment value on click of button', function () {
-  scope.value=10;
-  var button = compile(scope, angular.element('<div feature-else ></div>')).find('button');
-
-  button.triggerHandler('click');
-  scope.$digest();
-
-  expect(scope.value).toEqual(11);
+it('should remove element if feature if disabled', function () {
+  expect(enabledElement.parent().length).toBe(0);
 });
